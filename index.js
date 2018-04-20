7af07befd7f0084ea3f16b6e463993a160d568e4
leonardom249 <leonardo.megan@gmail.com>
2018-04-20 10:53:15 -0500

    added cheeselist to get endpoint

--- index.js
+++ index.js
@@ -10,6 +10,27 @@ const { dbConnect } = require('./db-mongoose');
 
 const app = express();
 
+const cheeses = [
+  'Bath Blue',
+  'Barkham Blue',
+  'Buxton Blue',
+  'Cheshire Blue',
+  'Devon Blue',
+  'Dorset Blue Vinney',
+  'Dovedale',
+  'Exmoor Blue',
+  'Harbourne Blue',
+  'Lanark Blue',
+  'Lymeswold',
+  'Oxford Blue',
+  'Shropshire Blue',
+  'Stichelton',
+  'Stilton',
+  'Blue Wensleydale',
+  'Yorkshire Blue'
+];
+
+
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
@@ -21,6 +42,10 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+
+app.get('/api/test', (req, res)=>{
+  res.json(cheeses);
+});
 
 function runServer(port = PORT) {
   const server = app
